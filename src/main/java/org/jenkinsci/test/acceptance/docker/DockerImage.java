19a24cdc331cd1a90ac4985726df2370457b32ff
Andrew Bayer <andrew.bayer@gmail.com>
2016-06-17 07:26:48 -0700

    Trying a longer timeout waiting for CID file

--- src/main/java/org/jenkinsci/test/acceptance/docker/DockerImage.java
+++ src/main/java/org/jenkinsci/test/acceptance/docker/DockerImage.java
@@ -116,7 +116,7 @@ public class DockerImage {
     }
 
     private String waitForCid(CommandBuilder docker, File cidFile, File logfile, Process p) throws InterruptedException, IOException {
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 20; i++) {
             Thread.sleep(500);
 
             String cid = FileUtils.readFileToString(cidFile);
